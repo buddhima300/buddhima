4cac956efc7dcc6d3ad801b8fd7128ea67cde204
buddhima chathuranga <buddhimachathuranga300@gmail.com>
2025-01-17 20:35:24 +0530

    modifying the body

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Body = () => {
   return (
     <div>
-      <div className="w-full bg-blue-200 h-[20cm] bg-[url('https://png.pngtree.com/thumb_back/fh260/background/20230722/pngtree-software-developer-conducting-quality-check-on-3d-gaming-software-and-application-image_3771481.jpg')] bg-cover">
-        <div className="absolute text-white my-[20%] mx-[10%]">
+      <div className="w-screen bg-blue-200 h-[20cm] bg-[url('https://png.pngtree.com/thumb_back/fh260/background/20230722/pngtree-software-developer-conducting-quality-check-on-3d-gaming-software-and-application-image_3771481.jpg')] bg-cover">
+        <div className="absolute text-white ml-[20%] mt-[20%] w-fit">
           <h3 className="text-gray-200">Make Your Strategy strong</h3>
           <h1 className="text-6xl font-bold">EXPERT SOLUTIONS</h1>
           <p className="text-left mt-3">
@@ -12,10 +12,10 @@ const Body = () => {
             nkfsidufi
           </p>
           <div className="flex flex-row mt-3">
-            <button className="bg-blue-600 text-white p-3 rounded-md w-auto mr-3">
+            <button className="bg-blue-600 text-white p-2 rounded-md w-fit mr-3">
               Get Started
             </button>
-            <button className="border-2 rounded-md p-3 ">About Us</button>
+            <button className="border-2 rounded-md p-2 ">About Us</button>
           </div>
         </div>
       </div>
