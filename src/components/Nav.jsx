f5f9302c1e25051f1d16aabdbabe4f51459a46ae
buddhima chathuranga <buddhimachathuranga300@gmail.com>
2025-01-17 20:02:24 +0530

    updating Navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,13 +8,14 @@ function Nav() {
     <div>
       <nav className="bg-blue-900 w-full sticky">
         {/* left side of navbar with logo */}
-        <div className="grid grid-cols-5 gap-5">
-          <div className="flex flex-row items-center">
-            <img src={Logo} alt="logo" srcset="" className="h-20" />
-            <h2 className="text-3xl font-bold text-white">FinBiz</h2>
+        <div className="flex flex-col gap-5 px-5 md:flex-row space-x-10 justify-between">
+          <div className="flex flex-col md:flex-row items-center text-white text-4xl font-medium">
+            <img src={Logo} alt="logo" srcset="" className="md:h-20" />
+            FinBiz
+            {/* <h2 className="text-3xl font-bold text-white">FinBiz</h2> */}
           </div>
           {/* Middle of the Navbar with links */}
-          <div className=" col-span-3 ml-10 md:flex flex-row gap-4 font-semibold items-center text-white justify-end">
+          <div className="col-span-3 flex flex-col md:flex-row gap-4 font-semibold items-center text-white justify-end">
             <li class="nav-item list-none">
               <a class="nav-link active" aria-current="page" href="#">
                 Home
@@ -62,12 +63,16 @@ function Nav() {
             </li>
           </div>
           {/* Right side of Nav bar */}
-          <div className="hidden lg:flex flex-row items-center">
-            <button className="text-white bg-blue-600 rounded-md p-2 mr-2 font-semibold">
+          <div className="flex justify-center items-center">
+            <button className="text-white w-max bg-blue-600 rounded-md p-2 mr-2 font-semibold">
               Book a Meeting
             </button>
-            <i class="bx bxs-phone-call text-blue-900 text-4xl bg-white rounded-md"></i>
-            <div className="flex flex-col text-white ml-2">
+          </div>
+          <div className=" text-white flex flex-col py-3">
+            <div className="flex justify-center">
+              <i class="bx bxs-phone-call text-blue-900 text-4xl bg-white rounded"></i>
+            </div>
+            <div className="grid grid-flow-row justify-center">
               <h3>Call us Anytime</h3>
               <p>+94 558555655</p>
             </div>
